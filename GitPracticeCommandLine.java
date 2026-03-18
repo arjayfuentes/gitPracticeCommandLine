3fa54ce6c694dbe32e8f007e60582b04538b5139
Arjay Fuentes <arjay630@gmail.com>
2026-03-18 13:47:09 +0100

    commitiing files after merge

--- GitPracticeCommandLine.java
+++ GitPracticeCommandLine.java
@@ -3,5 +3,7 @@ public class GitPracticeCommandLine {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World 2");
+
+        System.out.println("This is the change from feature branch");
     }
 }
